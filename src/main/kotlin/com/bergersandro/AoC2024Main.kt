d004363eae3caceac3cd70028a5d97eb6722903d
Berger Sandro <9106201+SandroBerger@users.noreply.github.com>
2024-12-03 16:42:02 +0100

    chore: include total execution time

--- src/main/kotlin/com/bergersandro/AoC2024Main.kt
+++ src/main/kotlin/com/bergersandro/AoC2024Main.kt
@@ -6,17 +6,27 @@ import kotlin.reflect.full.createInstance
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    val days = Reflections(AoCDay::class.java.`package`.name)
-        .getSubTypesOf(AoCDay::class.java)
-        .mapNotNull { it.kotlin.createInstance() }
-        .sortedBy { it::class.simpleName }
+    val days = Reflections(AoCDay::class.java.`package`.name).getSubTypesOf(AoCDay::class.java)
+        .mapNotNull { it.kotlin.createInstance() }.sortedBy { it::class.simpleName }
 
-    days.forEach {
+    val totalExecutionTime = days.sumOf {
         val executionTime = measureTimeMillis {
-            println("------------ ${it.javaClass.simpleName} ------------")
-            println("--> Solution part 1: ${it.part1()}")
-            println("--> Solution part 2: ${it.part2()}")
+            println(
+                """
+                ------------ ${it.javaClass.simpleName} ------------
+                --> Solution part 1: ${it.part1()}
+                --> Solution part 2: ${it.part2()}
+                """.trimIndent()
+            )
         }
         println("Execution time: $executionTime ms")
+        executionTime
     }
+
+    println(
+        """
+        ----------------------------------------------
+        "Total execution time: $totalExecutionTime ms"
+        """.trimIndent()
+    )
 }
